48d0af80236c10d48c9e1c1a93f189c37815d66a
wangzp <likelyzz@163.com>
2020-12-02 22:29:56 +0800

    debug

--- QuantumultX/jss/Notobiji.js
+++ QuantumultX/jss/Notobiji.js
@@ -8,14 +8,19 @@ var body = $response.body;
 var url  = $request.url;
 const path = "v1/subscribers/$RCAnonymousID:5958c79bfcf04c55b0561eb7d608328e";
 
-if (url.indexOf(path) != -1) {
-	console.log(body);
-	var obj = JSON.parse(body);
-	obj.subscriber.entitlements.pro.expires_date = "2020-12-09T06:15:03Z";
+console.log(body);
+var obj = JSON.parse(body);
+
+if(obj["subscriber"]!=null){
+	obj.subscriber.entitlements.pro.expires_date = "2021-01-09T06:15:03Z";
 	obj.subscriber.entitlements.pro.product_identifier = "com.lkzhao.editor.pro.ios.monthly";
-	obj.subscriber.subscriptions.com.lkzhao.editor.pro.ios.monthly.expires_date = "2020-12-09T06:15:03Z";
-//	obj.subscriber.subscriptions.com.lkzhao.editor.pro.ios.monthly.period_type = "trial";
+	obj.subscriber.subscriptions.com.lkzhao.editor.pro.ios.monthly.expires_date = "2021-01-09T06:15:03Z";
+	obj.subscriber.subscriptions.com.lkzhao.editor.pro.ios.monthly.period_type = "offer";
+
+    console.log("修改后： "+body);
 }
+
+
 
 body = JSON.stringify(obj);
 $done({body})	
