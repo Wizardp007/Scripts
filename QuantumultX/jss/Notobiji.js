6b56182bd39e8e2d95559f1419028c556177fd86
wangzp <likelyzz@163.com>
2020-12-02 21:49:02 +0800

    debug

--- QuantumultX/jss/Notobiji.js
+++ QuantumultX/jss/Notobiji.js
@@ -9,10 +9,11 @@ var url  = $request.url;
 const path = "v1/subscribers/$RCAnonymousID:5958c79bfcf04c55b0561eb7d608328e";
 
 if (url.indexOf(path) != -1) {
+	console.log(body);
 	var obj = JSON.parse(body);
-	obj.subscriber.entitlements.pro.expires_date = "2021-12-05T06:15:03Z";
+	obj.subscriber.entitlements.pro.expires_date = "2020-12-09T06:15:03Z";
 	obj.subscriber.entitlements.pro.product_identifier = "com.lkzhao.editor.pro.ios.monthly";
-	obj.subscriber.subscriptions.com.lkzhao.editor.pro.ios.monthly.expires_date = "2021-12-05T06:15:03Z";
+	obj.subscriber.subscriptions.com.lkzhao.editor.pro.ios.monthly.expires_date = "2020-12-09T06:15:03Z";
 //	obj.subscriber.subscriptions.com.lkzhao.editor.pro.ios.monthly.period_type = "trial";
 }
 
