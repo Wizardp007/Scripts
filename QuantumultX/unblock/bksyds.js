23ff71a9b31e4b494b1fb09ff5669f3ddf07ebcf
Vini <likelyzzw@gmail.com>
2024-06-12 11:40:19 +0800

    Update bksyds.js

--- QuantumultX/unblock/bksyds.js
+++ QuantumultX/unblock/bksyds.js
@@ -1,7 +1,18 @@
 var body = $response.body; 
 var obj = JSON.parse(body); 
+let url = $request.url;
+const cons1= "https://photoby.hasmash.com/app/browseEvent";
+const cons2= "https://photoby.hasmash.com/auth/member";
 
-obj.result.isVip=1;
+if(url === cons1){
+  obj.result.isVip=1;
+}
+
+if(url === cons2){
+  obj.result.memberExpire = 1907465936000;
+}
+
+
 
 body = JSON.stringify(obj); 
 $done(body); 
