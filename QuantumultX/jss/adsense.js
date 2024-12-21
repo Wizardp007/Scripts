3ef2dfa749978fe3378e4d59e942d2334ec2e151
Vini <likelyzzw@gmail.com>
2024-12-21 19:43:23 +0800

    Update adsense.js

--- QuantumultX/jss/adsense.js
+++ QuantumultX/jss/adsense.js
@@ -26,12 +26,15 @@ if ((url.includes("api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk")
         }
     } else {
         console.log(Object.keys(body));
-        body = {
-            "request_id": 'F5617E54-3FF4-4052-9B09-4227D09B5105',
-            "status_code": 20001,
-            "reason": 112,
-            "desc": "该代码位请求量过大且消耗过低，因此填充率控制在10%以内，该策略每日生效，如果当天该代码位的消耗上涨或请求量小于5000，则次日不会命中该策略"
-        };
+        body["status_code"] = 20001;
+        body["reason"] = 112;
+        body["desc"] = "该代码位请求量过大且消耗过低，因此填充率控制在10%以内，该策略每日生效，如果当天该代码位的消耗上涨或请求量小于5000，则次日不会命中该策略";
+      //  body = {
+      //      "request_id": 'F5617E54-3FF4-4052-9B09-4227D09B5105',
+      //      "status_code": 20001,
+      //      "reason": 112,
+      //      "desc": "该代码位请求量过大且消耗过低，因此填充率控制在10%以内，该策略每日生效，如果当天该代码位的消耗上涨或请求量小于5000，则次日不会命中该策略"
+      //  };
         console.log('成功');
     }
 } else if (url.includes('mi.gdt.qq.com') && method === getMethod) {
